47f3f5163f3516683f4874e182f6b9320cc6576d
Samyak Khatua <samyakkhatua@duck.com>
2023-01-15 23:43:23 +0530

    comming soon added

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="bg-gradient-to-b from-slate-900 via-slate to-black">
+      <div className=" bg-gradient-to-b from-slate-900 via-slate to-black">
         <div className="flex flex-col justify-center items-center">
           <h1 class="mb-4 mt-64 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-purple-800 from-indigo-500">
             Hey, I'm Samyak!
