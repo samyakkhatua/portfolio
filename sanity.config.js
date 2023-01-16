01091fcb723f99074088096412ef2ee7bfe6d8c1
Samyak Khatua <samyakkhatua@duck.com>
2023-01-17 00:24:13 +0530

    sanity configured

--- sanity.config.js
+++ sanity.config.js
@@ -3,12 +3,16 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'portfolio',
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-  projectId: 'k6njqb1v',
-  dataset: 'production',
+export default defineConfig({
+  basePath: "/studio",
+  name: 'samyak_content_studio',
+  title: 'samyak content studio',
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
