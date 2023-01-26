a039d9f47d1ace7e7e776f9bf4202b49c0f5051b
Samyak Khatua <samyakkhatua@duck.com>
2023-01-26 23:50:33 +0530

    sanity preview mode implemented

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,10 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
-
+// export default function preview(res: NextApiRequest, req:NextApiResponse) {
+//     res.setPreviewData({});
+//     res.writeHead(307, { Location: "/" });
+//     res.end();
+// }
 
 export default function preview(req, res) {
     res.setPreviewData({})
