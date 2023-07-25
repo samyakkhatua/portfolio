2db8ab646017f7ca5e4e6ec1ae3dfb392bfa850f
samyakkhatua <samyakkhatua@duck.com>
2023-07-25 23:54:13 +0530

    bug fixed

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Example } from "../components/example";
+
+export default function Head() {
+  return (
+    <>
+      <title className="text-black">Hi, I am Samyak!</title>
+
+      <h1 className="text-black">Samyak</h1>
+
+      <Example />
+    </>
+  )
+}
